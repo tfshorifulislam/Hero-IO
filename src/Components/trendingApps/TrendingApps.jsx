e22e28fee2b50ccade0a749b79eeaf373673aa7a
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-11 16:53:09 +0600

    tranding apps section heading add

---
+++ src/Components/trendingApps/TrendingApps.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TrendingApps = () => {
+    return (
+        <div className='mt-5 md:mt-20'>
+           <div className='flex flex-col justify-center items-center space-y-5'>
+            <h1 className='font-bold text-[#001931] text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center'>
+                Trending Apps
+            </h1>
+            <p className='text-lg text-[#627382] text-center'>
+                Explore All Trending Apps on the Market developed by us
+
+            </p>
+           </div>
+        </div>
+    );
+};
+
+export default TrendingApps;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import StaticSection from '../../Components/staticSection/StaticSection';
+import TrendingApps from '../../Components/trendingApps/TrendingApps';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <StaticSection />
+            <TrendingApps />
         </div>
     );
 };
