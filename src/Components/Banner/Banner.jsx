5138831d1532bf420c90b27ff2065b85256add0b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-11 16:04:02 +0600

    banner section done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-
+import banner from '../../assets/hero.png'
 const Banner = () => {
     return (
-        <div>
-            <div className='flex justify-center items-center'>
-                <div className='space-y-4 text-center'>
-                    <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:7xl text-[#001931]'>
-                        We Build <br /><span className='text-[#9F62F2]'>Productive Apps</span>
-                    </h1>
-                    <p className='text-[#627382] leading-8'>
-                        At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.
-                    </p>
-                </div>
+        <div className='flex flex-col-reverse md:flex-col  justify-center items-center'>
+            <div className='space-y-4 text-center'>
+                <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:7xl text-[#001931]'>
+                    We Build <br /><span className='text-[#9F62F2]'>Productive Apps</span>
+                </h1>
+                <p className='text-[#627382] md:leading-8'>
+                    At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.
+                </p>
+            </div>
+            <div className='md:mt-10 mb-4'>
+                <img src={banner} alt="" />
             </div>
         </div>
     );
