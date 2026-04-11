bad6c58601adba7711e521d5ef7ee4a59ded6e11
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-11 13:48:51 +0600

    navbar function add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
+
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -10,33 +12,19 @@ const Navbar = () => {
                     </div>
                     <ul
                         tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow items-center">
+                        <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Home</NavLink></li>
+                        <li><NavLink to={'/Apps'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Apps</NavLink></li>
+                        <li><NavLink to={'/Installation'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Installation</NavLink></li>
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul className="menu menu-horizontal px-1 items-center">
+                    <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Home</NavLink></li>
+                    <li><NavLink to={'/Apps'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Apps</NavLink></li>
+                    <li><NavLink to={'/Installation'} className={({ isActive }) => isActive ? 'text-purple-600 ' : ''}>Installation</NavLink></li>
                 </ul>
             </div>
             <div className="navbar-end">
