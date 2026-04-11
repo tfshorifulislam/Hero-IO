77c0188ee2ce228f52730c98354f57dbea9446ef
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-12 02:25:04 +0600

    apps section dynamic data add done

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import UseAppsData from '../../useAppsData/UseAppsData';
+import { HashLoader } from 'react-spinners';
+import AllCard from '../../ui/AllCard/AllCard';
+import { Link } from 'react-router';
 
 const Apps = () => {
+    const { app, loading } = UseAppsData()
     return (
-        <div>
-            <h1>this is apps page</h1>
+        <div className='mt-5 md:mt-20'>
+            <div className='flex flex-col justify-center items-center space-y-3 md:space-y-5 mb-4 md:mb-10'>
+                <h1 className='font-bold text-[#001931] text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center'>
+                    Trending Apps
+                </h1>
+                <p className='text-lg text-[#627382] text-center'>
+                    Explore All Trending Apps on the Market developed by us
+
+                </p>
+            </div>
+            {
+                loading ? <div className='flex justify-center items-center h-screen'><HashLoader color='#632EE3' /></div> :
+                    <div
+                        className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 w-11/12 mx-auto'>
+                        {
+                            app.map(card => <AllCard card={card} />)
+                        }
+                    </div>
+            }
         </div>
     );
 };
