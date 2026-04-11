206e678238d9611832fcf4525e23646a35979949
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-11 13:50:39 +0600

    navbar update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 
 const Layout = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto max-w-480'>
             <Navbar />
             <Outlet />
         </div>
