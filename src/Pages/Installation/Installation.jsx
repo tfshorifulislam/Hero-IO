227a04f7c133ed2b596dcd4525d1480ae5e5c3ab
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-12 18:30:27 +0600

    instalation page header add

--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -1,15 +1,30 @@
 import React, { useContext } from 'react';
 import { appContext } from '../../Context/InstalledAppsContext';
+import InstallationCard from './InstallationCard';
 
 const Installation = () => {
 
     const { install, setInstall } = useContext(appContext)
     console.log(install)
-       
+
     return (
         <div>
+            <div className='flex flex-col justify-center text-center'>
+                <h1
+                    className='font-bold text-5xl text-[#001931] mb-4'>
+                    Your Installed Apps
+                </h1>
+                <p className='text-[#627382] md:leading-8 text-xl mb-5 md:mb-10'>
+                    Explore All Trending Apps on the Market developed by us
+                </p>
+            </div>
+            <div>
+                <p className='text-2xl md:leading-8 text-[#001931] font-semibold '>
+                    {install.length} Apps Found
+                </p>
+            </div>
             {
-                install.map(i => <div>{i.title}</div>)
+                install.map(i => <InstallationCard key={i.id} i={i} />)
             }
         </div>
     );
---
+++ src/Pages/Installation/InstallationCard.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { appContext } from '../../Context/InstalledAppsContext';
+
+const InstallationCard = ({ i }) => {
+    const { install, setInstall } = useContext(appContext)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default InstallationCard;
