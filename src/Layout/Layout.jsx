490511678293dd75a1f3b7b32f4216165ecdee87
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-12 10:46:56 +0600

    footer design done

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../Pages/Footer/Footer';
 
 const Layout = () => {
     return (
         <div className='max-w-480'>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
