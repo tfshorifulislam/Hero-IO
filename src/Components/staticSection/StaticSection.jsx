29c95874aaa72dcc6441b7e4f2060045a8278bd2
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-13 15:56:24 +0600

    static section update

--- src/Components/staticSection/StaticSection.jsx
+++ src/Components/staticSection/StaticSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const StaticSection = () => {
     return (
-        <div className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] flex flex-col justify-center items-center py-20 px-10'>
+        <div className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] flex flex-col justify-center items-center py-8 px-5 md:py-20 md:px-10'>
             <div>
-                <h1 className='text-white font-bold text-xl md:text-5xl text-center mb-5 md:mb-10'>Trusted by Millions, Built for You</h1>
+                <h1 className='text-white font-semibold text-xl md:text-5xl text-center mb-5 md:mb-10'>Trusted by Millions, Built for You</h1>
             </div>
             <div className='flex gap-10 md:gap-20'>
                 <div className='font-extrabold text-2xl md:text-6xl text-white md:leading-16'>
